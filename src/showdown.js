0d00f62f36a2aead899461a6fe77810840c9f9e3
cenci0 <alchimist94@gmail.com>
2014-05-07 21:38:50 +0200

    Code tag patched, youtube tag replaced with video tag
    - No more parsed bbcode within code tag
    
    - Youtube tag is deprecated, replaced with video tag
    
    - Syntax page updated
    
    - WiP: music and twitter tag in progress

--- src/showdown.js
+++ src/showdown.js
@@ -4,18 +4,18 @@
 // Copyright (c) 2007 John Fraser.
 //
 // Original Markdown Copyright (c) 2004-2005 John Gruber
-// <http://daringfireball.net/projects/markdown/>
+//   <http://daringfireball.net/projects/markdown/>
 //
 // Redistributable under a BSD-style open source license.
 // See license.txt for more information.
 //
 // The full source distribution is at:
 //
-// A A L
-// T C A
-// T K B
+//				A A L
+//				T C A
+//				T K B
 //
-// <http://www.attacklab.net/>
+//   <http://www.attacklab.net/>
 //
 
 //
@@ -23,26 +23,27 @@
 // of the Perl version of Markdown.
 //
 // This is not a normal parser design; it's basically just a
-// series of string substitutions. It's hard to read and
-// maintain this way, but keeping Showdown close to the original
+// series of string substitutions.  It's hard to read and
+// maintain this way,  but keeping Showdown close to the original
 // design makes it easier to port new features.
 //
 // More importantly, Showdown behaves like markdown.pl in most
-// edge cases. So web applications can do client-side preview
+// edge cases.  So web applications can do client-side preview
 // in Javascript, and then build identical HTML on the server.
 //
 // This port needs the new RegExp functionality of ECMA 262,
-// 3rd Edition (i.e. Javascript 1.5). Most modern web browsers
-// should do fine. Even with the new regular expression features,
+// 3rd Edition (i.e. Javascript 1.5).  Most modern web browsers
+// should do fine.  Even with the new regular expression features,
 // We do a lot of work to emulate Perl's regex functionality.
 // The tricky changes in this file mostly have the "attacklab:"
-// label. Major or self-explanatory changes don't.
+// label.  Major or self-explanatory changes don't.
 //
 // Smart diff tools like Araxis Merge will be able to match up
-// this file with markdown.pl in a useful way. A little tweaking
+// this file with markdown.pl in a useful way.  A little tweaking
 // helps: in a copy of markdown.pl, replace "#" with "//" and
-// replace "$text" with "text". Be sure to ignore whitespace
+// replace "$text" with "text".  Be sure to ignore whitespace
 // and line endings.
+
 //
 
 
