8e11600243aaf45ba1536cc58ff3ba183b9a1d02
alfateam123 <alfateam123@gmail.com>
2014-05-12 10:11:55 +0000

    modify the "normal behaviour" test, add a new test for a "sane" handling of quotes inside lists

--- tests/test_lists.js
+++ tests/test_lists.js
@@ -12,14 +12,38 @@ loadTests([
 	options: null
 },
 {
-	description: "lists - quotes into lists",
+	description: "lists - quotes into lists - normal behaviour",
+	starting_text: [
+					"* Asagi\n* Charlotte \n* >Tsurugi\n>not goddess",
+					],
+	expected: [
+	            /*
+	            as you can see from the html code produced by Markdown Dingus,
+	            the </ul> tag is inside the </blockquote>. We have to consider
+	            this behaviour the "normal" one.
+
+	            <p><ul>
+				<li>Asagi</li>
+				<li>Charlotte </li>
+				<li>>Tsurugi <br /></p>
+
+				<blockquote>
+				  <p>not a goddess</li>
+				</ul></p>
+				</blockquote>
+	            */
+				"[list]\n[*] Asagi\n[*] Charlotte \n[*] >Tsurugi\n\n[quote]not goddess\n[/list]\n[/quote]",
+			  ],
+	options: null
+},
+{
+	description: "lists - quotes into lists - using check_quotes_into_lists",
 	starting_text: [
 					"* Asagi\n* Charlotte \n* >Tsurugi\n>not goddess",
 					],
 	expected: [
 				"[list]\n[*] Asagi\n[*] Charlotte \n[*] [quote]Tsurugi\nnot goddess\n[/quote]\n[/list]",
 			  ],
-	options: null
-},
-
+	options: {check_quotes_into_lists: true}
+}
 ]);
