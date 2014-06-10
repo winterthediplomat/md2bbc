7aa362bfc87c6a1a8947ed0a042f7b46cd680e07
alfateam123 <alfateam123@gmail.com>
2014-06-10 17:57:22 +0200

    add build scripts
    add a Python script dedicated to the build of the minified version of showdown.js and to the rewriting of the test pages

---
+++ build/build.py
@@ -0,0 +1,46 @@
+import httplib, urllib, sys
+from os import path, walk
+from string import Template
+
+def minify_js(readablejs_path, minifiedjs_path):
+    #adapted from the Closure Compiler documentation
+    # Define the parameters for the POST request and encode them in
+    # a URL-safe format.
+
+    params = urllib.urlencode([
+        ('js_code', open(readablejs_path).read()),
+        ('compilation_level', 'SIMPLE'),
+        ('output_format', 'text'),
+        ('output_info', 'compiled_code'),
+      ])
+
+    # Always use the following value for the Content-type header.
+    headers = { "Content-type": "application/x-www-form-urlencoded" }
+    conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+    conn.request('POST', '/compile', params, headers)
+    response = conn.getresponse()
+    data = response.read()
+    open(minifiedjs_path, "w").write(data);
+    conn.close()
+
+def build_testpage(js_path):
+    ignorelist = open("test_ignorelist.txt").read().splitlines()
+    for act_path, dirs_, files_ in walk("../tests"):
+        js_files = [path.join(act_path, jsf) for jsf in files_ if jsf.endswith(".js") and jsf not in ignorelist]
+    testpage_template = Template(open("testpage.tmpl").read())
+    return testpage_template.safe_substitute(
+        JS_PATH = js_path,
+        TESTLOAD_LIST = "\n".join(
+            "<script type='text/javascript' src='"+testpath+"'></script>"
+            for testpath in js_files
+            )
+        )
+    
+
+if __name__=="__main__":
+    readablejs_path = '../src/showdown.js'
+    minifiedjs_path = "../src/showdown.min.js"
+    minify_js(readablejs_path, minifiedjs_path, proxy_data)
+
+    open("../tests/test.html", "w").write(build_testpage(readablejs_path))
+    open("../tests/test_min.html", "w").write(build_testpage(minifiedjs_path))
