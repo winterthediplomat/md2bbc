9a60538479b83ee111813e53b3cd2835608beedd
alfateam123 <alfateam123@gmail.com>
2014-06-21 12:31:30 +0200

    now getting more informations from the compiler!
    When minifying the Charlotte code, the script now asks the Closure Compiler if our code raises warnings or is syntaxically (:P) broken.
    The errors are only notified, the code is always generated (unless build.py is called with --no-minify parameter)

--- build/build.py
+++ build/build.py
@@ -23,6 +23,32 @@ def minify_js(readablejs_path, minifiedjs_path):
     open(minifiedjs_path, "w").write(data);
     conn.close()
 
+def is_js_valid(readablejs_path):
+    #adapted from the Closure Compiler documentation
+    # Define the parameters for the POST request and encode them in
+    # a URL-safe format.
+    def askTheCompiler(readablejs_path, desired_output_info):
+        params = urllib.urlencode([
+            ('js_code', open(readablejs_path).read()),
+            ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+            ('output_format', 'text'),
+            ('output_info', desired_output_info),
+          ])
+
+        # Always use the following value for the Content-type header.
+        headers = { "Content-type": "application/x-www-form-urlencoded" }
+        conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+        conn.request('POST', '/compile', params, headers)
+        response = conn.getresponse()
+        data = response.read()
+        #open(minifiedjs_path, "w").write(data);
+        #print(data)
+        conn.close()
+        return data
+    results = [{outinfo: askTheCompiler(readablejs_path, outinfo)} for outinfo in ["warnings", "errors"]]
+    #print(results)
+    return {"result": any(k.values()[0]=="" for k in results), "responses":results}
+
 def build_testpage(js_path):
     ignorelist = open("test_ignorelist.txt").read().splitlines()
     js_files = []
@@ -45,7 +71,14 @@ if __name__=="__main__":
     minifiedjs_path = "../src/showdown.min.js"
 
     if len(sys.argv)==1 or sys.argv[1] != '--no-minify':
-        minify_js(readablejs_path, minifiedjs_path)
+        check = is_js_valid(readablejs_path)
+        if check["result"]:
+            print("the code is ok!")
+            minify_js(readablejs_path, minifiedjs_path)
+        else:
+            print("the showdown code is broken!")
+            for i in check["responses"]:
+                print(i)
 
     open("../tests/test.html", "w").write(build_testpage(readablejs_path))
     open("../tests/test_min.html", "w").write(build_testpage(minifiedjs_path))
